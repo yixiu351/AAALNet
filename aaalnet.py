2bb67c07d206b149f54b40b39ce69d2eb77645f5
yixiu351 <141032777+yixiu351@users.noreply.github.com>
2025-04-17 17:29:59 +0800

    AAALNet

--- aaalnet.py
+++ aaalnet.py
@@ -3,7 +3,6 @@
 import torch
 import torch.nn as nn
 from function import calc_mean_std, mean_variance_norm
-from Content_SA import Content_SA
 from whiten_transform import Whiten
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 decoder = nn.Sequential(
@@ -248,6 +247,33 @@ class ASA(nn.Module):
         O += content
         return O
 
+class Content_SA(nn.Module):
+    def __init__(self, in_dim):
+        super(Content_SA, self).__init__()
+        self.f = nn.Conv2d(in_dim, in_dim, (1, 1))
+        self.g = nn.Conv2d(in_dim, in_dim, (1, 1))
+        self.h = nn.Conv2d(in_dim, in_dim, (1, 1))
+        self.softmax = nn.Softmax(dim=-1)
+        self.out_conv = nn.Conv2d(in_dim, in_dim, (1, 1))
+        self.whiten=Whiten()
+    def forward(self, content_feat):
+        content_whiten=self.whiten(content_feat)
+        B, C, H, W = content_feat.size()
+        F_Fc_norm = self.f(mean_variance_norm(content_whiten)).view(B, -1, H * W).permute(0, 2, 1)
+
+        B, C, H, W = content_feat.size()
+        G_Fs_norm = self.g(mean_variance_norm(content_whiten)).view(B, -1, H * W)
+
+        energy = torch.bmm(F_Fc_norm, G_Fs_norm)
+        attention = self.softmax(energy)
+
+        H_Fs = self.h(content_feat).view(B, -1, H * W)
+        out = torch.bmm(H_Fs, attention.permute(0, 2, 1))
+        B, C, H, W = content_feat.size()
+        out = out.view(B, C, H, W)
+        out = self.out_conv(out)
+        out += content_feat
+        return out
 
 class Transform(nn.Module):
     def __init__(self, in_planes):
